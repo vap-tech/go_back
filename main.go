ae31c2fd8ce3948edc1be2d913a762f23949560d
v.petrenko <bafomet2016vitt@gmail.com>
2025-01-26 01:27:16 +0300

    add endpoint /status

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Server running")
+
+	// Echo instance
+	s := echo.New()
+	s.GET("/status", Handler)
+
+	err := s.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func Handler(ctx echo.Context) error {
+	err := ctx.String(http.StatusOK, "Hello World")
+	if err != nil {
+		return err
+	}
+	return nil
 }
